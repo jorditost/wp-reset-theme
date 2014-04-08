132cce94f27b4332abffd712f2aec8b21412f274
Guillaume Mutschler <guillaume.mutschler@gmail.com>
2014-04-08 11:15:17 +0200

    Speed up loading
    - Deleted pingback in the header
    - Removed rel link to the Xhtml Friends Network profile
    - Removed rel links to RSD service endpoint and to Windows Live Writer
    Manifest file

--- functions.php
+++ functions.php
@@ -425,6 +425,7 @@ function my_admin_scripts_method() {
 }
 //add_action('admin_print_scripts', 'my_admin_scripts_method');
 
+
 //////////////////////////////////////
 // keeps crawlers out of the dev env
 //////////////////////////////////////
@@ -442,18 +443,17 @@ add_action('init', 'robots_access');
     
 // Clean up the <head>
 function remove_head_links() {
-    // remove_action('wp_head', 'rsd_link');
-    // remove_action('wp_head', 'wlwmanifest_link');
+    remove_action('wp_head', 'rsd_link'); //Really Simple Discovery service: Used by 3rd party software posts (like phones wordpress app -> reactivate if needed)
+    remove_action('wp_head', 'wlwmanifest_link'); //Used by Windows Live Writer
     remove_action( 'wp_head', 'feed_links_extra', 3 ); // Display the links to the extra feeds such as category feeds
     remove_action( 'wp_head', 'feed_links', 2 ); // Display the links to the general feeds: Post and Comment Feed
-    //remove_action( 'wp_head', 'rsd_link' ); // Display the link to the Really Simple Discovery service endpoint, EditURI link
-    //remove_action( 'wp_head', 'wlwmanifest_link' ); // Display the link to the Windows Live Writer manifest file.
     remove_action( 'wp_head', 'index_rel_link' ); // index link
     remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 ); // prev link
     remove_action( 'wp_head', 'start_post_rel_link', 10, 0 ); // start link
     remove_action( 'wp_head', 'adjacent_posts_rel_link', 10, 0 ); // Display relational links for the posts adjacent to the current post.
     remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
     remove_action( 'wp_head', 'wp_generator' ); // Display the XHTML generator that is generated on the wp_head hook, WP version
+
 }
 add_action('init', 'remove_head_links');
 //remove_action('wp_head', 'wp_generator');
